19841cbe208033f71ffc856e6c354f1cdc25c5c6
Emilio Castillo <ecastill@preferred.jp>
2020-02-17 03:37:27 +0000

    Add ignite version  check >= 0.3.0

--- chainer_pytorch_migration/ignite/__init__.py
+++ chainer_pytorch_migration/ignite/__init__.py
@@ -1,2 +1,16 @@
+import ignite
+
 from .extensions import add_trainer_extension, load_chainer_snapshot  # NOQA
 from .collate import collate_to_array  # NOQA
+
+
+def _get_version(version):
+    # We compare up to the minor version (first two digits).
+    # This is because it is highly unlikely that these numbers
+    # will contain other character than digits.
+    return [int(x) for x in version.split('.')[:2]]
+
+
+if _get_version(ignite.__version__) < _get_version('0.3.0'):
+    raise ImportError('Ignite version found {}. '
+                      'Required is >=0.3.0'.format(ignite.__version__))
