Fix backprop of LinkAsTorchModel

chainer_pytorch_migration/parameter.py
@@ -1,9 +1,23 @@
-import chainer_pytorch_migration as cpm
+import chainer
 import torch
-from torch import nn
+
+import chainer_pytorch_migration as cpm
 
 
-class LinkAsTorchModel(nn.Module):
+def _named_children(link):
+    assert isinstance(link, chainer.Link)
+    if isinstance(link, chainer.Chain):
+        for name in link._children:
+            yield name, getattr(link, name)
+
+
+def _named_params(link):
+    assert isinstance(link, chainer.Link)
+    for name in link._params:
+        yield name, getattr(link, name)
+
+
+class LinkAsTorchModel(torch.nn.Module):
 
     '''Converts a Chainer Link to a PyTorch module.
 
@@ -16,9 +30,14 @@ class LinkAsTorchModel(nn.Module):
     '''
 
     def __init__(self, link):
-        super(LinkAsTorchModel, self).__init__()
-        for n, p in sorted(link.namedparams()):
-            self.__setattr__(n, ChainerParameter(p))
+        super().__init__()
+
+        for name, child in _named_children(link):
+            child_module = LinkAsTorchModel(child)
+            setattr(self, name, child_module)
+        for name, param in sorted(_named_params(link)):
+            setattr(self, name, ChainerParameter(param))
+
         self.link = link
 
     def forward(self, *input):
@@ -28,10 +47,50 @@ class LinkAsTorchModel(nn.Module):
         # The return value should be a tensor as well.
         input = [cpm.tensor.asarray(x) if isinstance(x, torch.Tensor)
                  else x for x in input]
-        return cpm.tensor.astensor(self.link.forward(*input).array)
+        outputs = self.link.forward(*input)
+        ret = self.__as_tensor(outputs)
+        return ret
+
+    def __as_tensor(self, value):
+        if isinstance(value, tuple):
+            return tuple(self.__as_tensor(x) for x in value)
+        if isinstance(value, list):
+            return [self.__as_tensor(x) for x in value]
+        if isinstance(value, chainer.Variable):
+            return _ChainerTensor(value)
+        return value
 
 
-class ChainerParameter(nn.Parameter):
+class _ChainerTensor(torch.Tensor):
+    '''
+    Torch tensor from which backprop can be performed.
+    '''
+    def __new__(cls, variable):
+        assert isinstance(variable, chainer.Variable)
+        obj = cpm.astensor(variable.array)
+        obj.__class__ = cls
+        return obj
+
+    def __init__(self, variable):
+        self._variable = variable
+
+    def backward(self, gradient=None, retain_graph=None, create_graph=False):
+        assert retain_graph is None or retain_graph == False  # True not supported
+        assert self._variable is not None
+
+        var = self._variable
+        if gradient is not None:
+            var.grad = cpm.tensor.asarray(gradient)
+        var.backward(
+            enable_double_backprop=create_graph,
+        )
+
+    def zero_(self):
+        super().zero_()
+        self._variable = None
+
+
+class ChainerParameter(torch.nn.Parameter):
 
     '''Wraps a Chainer parameter for use with a PyTorch optimizer.
 
@@ -46,6 +105,8 @@ class ChainerParameter(nn.Parameter):
         A :class:`ChainerParameter`.
     '''
 
+    __grad = None
+
     def __new__(cls, param):
         return super().__new__(cls, cpm.astensor(param.array))
 
@@ -55,11 +116,10 @@ class ChainerParameter(nn.Parameter):
 
     @property
     def grad(self):
-        # TODO(hvy): Cache constructed `torch.Tensor`.
-        if self._param.grad is not None:
-            return cpm.astensor(self._param.grad)
-        else:
-            return None
+        if self.__grad is None:
+            if self._param.grad is not None:
+                self.__grad = _ChainerTensor(self._param.grad_var)
+        return self.__grad
 
     @grad.setter
     def grad(self, g):
@@ -67,3 +127,8 @@ class ChainerParameter(nn.Parameter):
             self.grad[...] = g
         else:
             self._param.grad = cpm.asarray(g)
+
+    def zero_(self):
+        super().zero_()
+        self._param = None
+        self.__grad = None

tests/test_parameter.py
@@ -73,7 +73,7 @@ def test_chainer_parameter_grad_setter(shape):
     numpy.testing.assert_array_equal(grad, torch_param.grad)
 
 
-def test_to_chainer_parameters():
+def test_link_as_torch_model():
     # initialized parameter
     a_arr = numpy.ones((3,  2), 'float32')
     a_chainer_param = chainer.Parameter(a_arr)
@@ -101,7 +101,72 @@ def test_to_chainer_parameters():
     assert (a_chainer_param.array == numpy.arange(6).reshape((3, 2))).all()
 
 
-def test_to_chainer_parameters_uninitialized():
+def test_link_as_torch_model_nested():
+    dtype = numpy.float32
+
+    # l2: MyLink2 := p2 * l1(x)
+    #   - p2
+    #   - l1: MyLink1 := p1 * x
+    #     - p1
+    class MyLink1(chainer.Link):
+        def __init__(self):
+            super().__init__()
+            with self.init_scope():
+                self.p1 = chainer.Parameter(numpy.array([2], dtype))
+        def forward(self, x1):
+            return self.p1 * x1
+
+    class MyLink2(chainer.Chain):
+        def __init__(self):
+            super().__init__()
+            with self.init_scope():
+                self.p2 = chainer.Parameter(numpy.array([3], dtype))
+                self.l1 = MyLink1()
+        def forward(self, x2):
+            return self.p2 * self.l1(x2)
+
+    # Dummy optimizer that always writes the constant value 9.
+    class MyOptim(torch.optim.Optimizer):
+        def __init__(self, params):
+            super().__init__(params, {})
+        def step(self):
+            for group in self.param_groups:
+                for param in group['params']:
+                    param.data[...] = 9
+
+    link = MyLink2()
+    module = cpm.LinkAsTorchModel(link)
+    assert isinstance(module.p2, torch.nn.Parameter)
+    assert isinstance(module.l1, torch.nn.Module)
+    assert isinstance(module.l1.p1, torch.nn.Parameter)
+    assert len(list(module.parameters(recurse=False))) == 1
+    assert len(list(module.l1.parameters(recurse=False))) == 1
+    assert len(list(module.parameters(recurse=True))) == 2
+
+    optimizer = MyOptim(module.parameters())
+    x = numpy.array([4], dtype)
+
+    # Forward
+    y = module(x)
+    assert isinstance(y, torch.Tensor)
+    numpy.testing.assert_array_equal(y.detach().numpy(), [24])
+
+    # Backward
+    y.backward()
+
+    numpy.testing.assert_array_equal(module.l1.p1.grad.detach().numpy(), [12])
+    numpy.testing.assert_array_equal(module.p2.grad.detach().numpy(), [8])
+
+    # Optimizer step
+    optimizer.step()
+
+    numpy.testing.assert_array_equal(link.p2.array, [9])
+    numpy.testing.assert_array_equal(link.l1.p1.array, [9])
+    numpy.testing.assert_array_equal(module.p2.detach().numpy(), [9])
+    numpy.testing.assert_array_equal(module.l1.p1.detach().numpy(), [9])
+
+
+def test_link_as_torch_model_uninitialized():
     # Uninitialized parameters are not supported
     a_chainer_param = chainer.Parameter()
 
@@ -128,10 +193,10 @@ def test_state_dict():
 
     torched = cpm.LinkAsTorchModel(link)
     state_dict = torched.state_dict()
-    assert '/a' in state_dict
-    numpy.testing.assert_array_equal(a_arr, state_dict['/a'].detach())
-    assert '/b' in state_dict
-    numpy.testing.assert_array_equal(b_arr, state_dict['/b'].detach())
+    assert 'a' in state_dict
+    numpy.testing.assert_array_equal(a_arr, state_dict['a'].detach())
+    assert 'b' in state_dict
+    numpy.testing.assert_array_equal(b_arr, state_dict['b'].detach())
 
 
 def test_named_params():
@@ -148,7 +213,7 @@ def test_named_params():
 
     torched = cpm.LinkAsTorchModel(link)
     n_params = dict(torched.named_parameters())
-    assert '/a' in n_params
-    numpy.testing.assert_array_equal(a_arr, n_params['/a'].detach())
-    assert '/b' in n_params
-    numpy.testing.assert_array_equal(b_arr, n_params['/b'].detach())
+    assert 'a' in n_params
+    numpy.testing.assert_array_equal(a_arr, n_params['a'].detach())
+    assert 'b' in n_params
+    numpy.testing.assert_array_equal(b_arr, n_params['b'].detach())