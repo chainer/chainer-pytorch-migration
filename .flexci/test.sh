57e0413d5d902cf198f37e36057b9439991e57f2
Toshiki Kataoka <kataoka@preferred.jp>
2020-02-25 20:12:27 +0900

    flexci: use cupy v7 to be compatible with chainer

--- .flexci/test.sh
+++ .flexci/test.sh
@@ -4,7 +4,7 @@ perl -pi.bak -e 's|http://archive\.ubuntu\.com/ubuntu/|mirror://mirrors.ubuntu.c
 apt update
 apt -y install python3 python3-pip
 
-pip3 install -U --pre chainer cupy-cuda100
+pip3 install -U chainer 'cupy-cuda100<8'
 pip3 install pytorch-ignite
 pip3 install -e '.[test]'
 
